1ee344dfb3ed397764cda9a80ad392336c770a13
Timotej979 <timotej.pet@gmail.com>
2025-06-02 10:54:38 +0200

    fix: Update gcp oidc config to match checkov req

--- configure-oidc/gcp/1-gcp-oidc.tf
+++ configure-oidc/gcp/1-gcp-oidc.tf
@@ -6,7 +6,19 @@ resource "google_iam_workload_identity_pool" "github_actions" {
 }
 
 resource "google_iam_workload_identity_pool_provider" "github_actions_provider" {
-    for_each = var.workload_identity_providers_config
+    for_each = {
+    for k, config in var.workload_identity_providers_config :
+    k => {
+      for workflow_ref in config.workflow_ref_claims :
+      "${k}-${basename(workflow_ref)}" => {
+        name             = config.name
+        actor_claim      = config.actor_claim
+        repository_claim = config.repository_claim
+        ref_claim        = config.ref_claim
+        workflow_file    = split("/", split("@", workflow_ref)[0])[3]  # Extract just "build-gcp.yml"
+      }
+    }
+  }
 
     workload_identity_pool_provider_id = "${each.value.name}-wip"
     workload_identity_pool_id          =  google_iam_workload_identity_pool.github_actions.workload_identity_pool_id
@@ -19,17 +31,17 @@ resource "google_iam_workload_identity_pool_provider" "github_actions_provider" 
     }
 
     attribute_condition = <<EOT
+        assertion.sub == "repo:${each.value.repository_claim}:ref:${each.value.ref_claim}:workflow:${each.value.workflow_file}" &&
         assertion.actor == "${each.value.actor_claim}" &&
         assertion.repository == "${each.value.repository_claim}" &&
-        assertion.ref == "${each.value.ref_claim}" &&
-        assertion.workflow_ref in ["${join("\", \"", each.value.workflow_ref_claims)}"]
+        assertion.ref == "${each.value.ref_claim}"
     EOT
 
+
     attribute_mapping = {
-        "google.subject"         = "assertion.sub"
-        "assertion.actor"        = "assertion.actor"
-        "assertion.repository"   = "assertion.repository"
-        "assertion.ref"          = "assertion.ref"
-        "assertion.workflow_ref" = "assertion.workflow_ref"
+        "google.subject"       = "assertion.sub"
+        "attribute.actor"      = "assertion.actor"
+        "attribute.repository" = "assertion.repository"
+        "attribute.ref"        = "assertion.ref"
     }
 }
