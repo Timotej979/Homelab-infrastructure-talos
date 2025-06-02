55c52ea467a505f9042ef8fa8a17239556b4f515
Timotej979 <timotej.pet@gmail.com>
2025-06-02 12:20:00 +0200

    fix: Try to fix checkov scan withgcp oidc

--- configure-oidc/gcp/1-gcp-oidc.tf
+++ configure-oidc/gcp/1-gcp-oidc.tf
@@ -1,19 +1,21 @@
 locals {
   github_actions_providers = merge(
     [
-        for k, config in var.workload_identity_providers_config : {
-            for workflow_ref in config.workflow_ref_claims : "${k}-${basename(workflow_ref)}" => {
-                name                = config.name
-                actor_claim         = config.actor_claim
-                repository_claim    = config.repository_claim
-                ref_claim           = config.ref_claim
-                workflow_file       = split("/", split("@", workflow_ref)[0])[3]
-                attribute_condition = "assertion.sub == \"repo:${config.repository_claim}:ref:${config.ref_claim}:workflow:${split("/", split("@", workflow_ref)[0])[3]}\""
-            }
+      for k, config in var.workload_identity_providers_config : {
+        for workflow_ref in config.workflow_ref_claims : 
+        "${k}-${last(split(workflow_ref, "/"))}" => {
+          name                = config.name
+          actor_claim         = config.actor_claim
+          repository_claim    = config.repository_claim
+          ref_claim           = config.ref_claim
+          workflow_file       = split("/", split("@", workflow_ref)[0])[3]
+          attribute_condition = "assertion.sub == \"repo:${config.repository_claim}:ref:${config.ref_claim}:workflow:${split("/", split("@", workflow_ref)[0])[3]}\""
         }
+      }
     ]...
   )
 }
+
 
 resource "google_iam_workload_identity_pool" "github_actions" {
     workload_identity_pool_id = "github-actions"
