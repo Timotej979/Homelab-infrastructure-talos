8d4081ddc938914c2c32a297f69d1147938cafaf
Timotej979 <timotej.pet@gmail.com>
2025-06-06 19:16:54 +0200

    feat: Add TF Alicloud support, draft for Tencent

--- terraform/gcp/2-outputs.tf
+++ terraform/gcp/2-outputs.tf
@@ -1,12 +1,12 @@
 output "workload_identity_pool_id" {
-  description = "The Workload Identity Pool ID"
-  value       = google_iam_workload_identity_pool.github_actions.workload_identity_pool_id
+    description = "The Workload Identity Pool ID"
+    value       = google_iam_workload_identity_pool.github_actions.workload_identity_pool_id
 }
 
 output "workload_identity_pool_provider_ids" {
-  description = "Map of Workload Identity Provider IDs"
-  value = {
-    for k, provider in google_iam_workload_identity_pool_provider.github_actions_provider :
-    k => provider.workload_identity_pool_provider_id
-  }
+    description = "Map of Workload Identity Provider IDs"
+    value = {
+        for k, provider in google_iam_workload_identity_pool_provider.github_actions_provider : 
+            k => provider.workload_identity_pool_provider_id
+    }
 }
